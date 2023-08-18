fc5316ee9182cf753295a923ddfb187c352c736b
HelioSchilipak <42068577+HelioSchilipak@users.noreply.github.com>
2023-08-18 16:58:18 -0300

    Identação / Class / PHP

---
+++ ferramentas.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="./coat.team/img/faviconmain.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COAT TEAM 2023</title>
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <nav>
+            <?php include './html/navindex.html'; ?>
+        </nav>
+
+
+        <header class="ferramentas">
+        <p>
+        //teste
+        </p>
+                
+        </header>
+        
+        <main>
+            
+
+        </main>
+        
+        <footer>
+            <?php include './html/footer.html'; ?>
+        </footer>
+
+    </div>
+    
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="./img/faviconmain.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COAT TEAM 2023</title>
+    <link rel="stylesheet" href=".\css\index.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <nav>
+            <?php include './html/navindex.html'; ?>
+        </nav>
+
+        <header>            
+            <img src="./img/coat.team.logo.png" alt="coat.team">                      
+        </header>
+        
+        <main>
+            <p>            
+
+            </p>  
+
+        </main>
+        
+        <footer>
+            <?php include './html/footer.html'; ?>
+        </footer>
+
+    </div>
+    
+</body>
+</html>
+
